07d01bdf8df13e199e2190d86f6660b24903d9b3
Vitridax <vitridax@gmail.com>
2018-04-14 12:19:58 -0700

    wires can carry power, more room tiles.

--- scripts/wire-puzzles.js
+++ scripts/wire-puzzles.js
@@ -24,12 +24,14 @@ gameScene.init = function() {
     this.previousSelectedTilePos = {x: 0, y: 0};
     this.mapWidth = 10;
     this.mapHeight = 8;
+    this.debugModeEnabled = true;
 }
 
 
 //load game assets
 gameScene.preload = function() {
     this.load.image('room tiles',           'images/room-tiles.png');
+    this.load.spritesheet('room spritesheet', 'images/room-tiles.png', {frameWidth: this.tileSize, frameHeight: this.tileSize});
     this.load.image('powered wire tiles',   'images/powered-wire-tiles.png');
     this.load.image('unpowered wire tiles', 'images/unpowered-wire-tiles.png');
     this.load.image('white wire tiles', 'images/white-wire-tiles.png');
@@ -84,12 +86,21 @@ gameScene.create = function() {
         height: this.mapHeight 
     });
 
-    var roomTiles = this.map.addTilesetImage('room tiles');
+    var roomTileset = this.map.addTilesetImage('room tiles');
+    this.roomTiles = {
+        floor: 0,
+        wall: 1,
+        output: 2,
+        input: 3,
+        hardTWall: 4, //toggleable wall
+        softTWall: 5
+    }
+    this.drawTile = this.roomTiles.wall;
     var poweredWireTiles = this.map.addTilesetImage('powered wire tiles');
     var unpoweredWireTiles = this.map.addTilesetImage('unpowered wire tiles');
     var whiteWireTiles = this.map.addTilesetImage('white wire tiles');
 
-    var ground = this.map.createBlankDynamicLayer('room', roomTiles);
+    var ground = this.map.createBlankDynamicLayer('room', roomTileset);
     var wires = this.map.createBlankDynamicLayer('wires', whiteWireTiles);
     ground.fill(1);
     ground.fill(0, 1, 1, this.map.width-2, this.map.height-2);
@@ -102,6 +113,41 @@ gameScene.create = function() {
     this.marker = this.add.graphics();
     this.marker.lineStyle(3, 0x000000, 1);
     this.marker.strokeRect(0, 0, this.map.tileWidth*(this.tileScale+1), this.map.tileHeight*(this.tileScale+1));
+
+    //pick tile gui
+    this.tile = this.add.sprite(900, 40, 'room spritesheet', 2);
+    let scaledTileSize = this.tileSize*(this.tileScale+1);
+    let mapEdge = this.map.tileToWorldX(this.map.width) + 50;
+    var tileSelectGroup = this.add.group([
+        {
+            key: 'room spritesheet',
+            frame: [0, 1, 2],
+            setXY: { x: mapEdge, y: 0, stepX: scaledTileSize },
+            setScale: { x: this.tileScale+1, y: this.tileScale+1 }
+        }, 
+        {
+            key: 'room spritesheet',
+            frame: [3, 4, 5],
+            setXY: { x: mapEdge, y: 0+scaledTileSize, stepX: scaledTileSize },
+            setScale: { x: this.tileScale+1, y: this.tileScale+1 }
+        }
+    ]);
+    // Phaser.Actions.SetOrigin(tileSelectGroup.getChildren(), 0, 0);
+    Phaser.Actions.IncXY(tileSelectGroup.getChildren(), scaledTileSize/2, scaledTileSize/2);
+    Phaser.Actions.Call(tileSelectGroup.getChildren(), function(tile) {
+        tile.setInteractive();
+        tile.on("pointerdown", function(pointer) {
+            this.scene.drawTile = this.frame.name;
+            this.scene.selectedTileMarker.setPosition(this.getTopLeft().x, this.getTopLeft().y);
+            // this.scene.selectedTileMarker.setPosition(100, 100);
+        });
+    }, this);
+
+    let selectedTileCoords = tileSelectGroup.getChildren()[this.drawTile].getTopLeft();
+    this.selectedTileMarker = this.add.graphics();
+    this.selectedTileMarker.lineStyle(5, 0x000000, 1);
+    this.selectedTileMarker.strokeRect(0, 0, this.map.tileWidth*(this.tileScale+1), this.map.tileHeight*(this.tileScale+1));
+    this.selectedTileMarker.setPosition(selectedTileCoords.x, selectedTileCoords.y);
 
     //player
     this.player = this.add.sprite(this.map.tileToWorldX(2.5), this.map.tileToWorldY(2.5), 'player');
@@ -123,13 +169,16 @@ gameScene.create = function() {
             dy *= (this.directionMoving === 'N') ? -1 : 1;
             this.y += dy;
         }
-        else if (this.x === tileX && this.y === tileY) this.directionMoving = '';
+        else if (this.x === tileX && this.y === tileY) {
+            this.directionMoving = '';
+        };
         // if (this.directionMoving === 'N') {
         //     if(this.x != tileX)
         // }
     }
 
     this.player.move = function(dir) {
+        //update all wires
         if (this.directionMoving !== '') return;
         let dx = 0;
         let dy = 0;
@@ -142,7 +191,9 @@ gameScene.create = function() {
         let tileX = this.scene.map.worldToTileX(this.x)+dx;
         let tileY = this.scene.map.worldToTileY(this.y)+dy;
         let tile =this.scene.map.getTileAt(tileX, tileY, true, 'room');
-        if (tile === null || tile.index === 1) return; //will hit wall;
+        if (tile === null ||
+            !(tile.index === this.scene.roomTiles.floor || 
+              tile.index === this.scene.roomTiles.softTWall)) return; //will hit wall;
 
         this.directionMoving = dir;
         this.x += this.speed*dx;
@@ -154,21 +205,12 @@ gameScene.create = function() {
 
 gameScene.update = function(time, delta) {
 
-    if (this.keys.Q.isDown) {
-        this.map.forEachTile(function(tile) {
-            if (tile.index === -1) return;
-            // console.log(tile);
-            // tile.tint += 100;
-            console.log(tile.tint);
-        });
-    }
-
     // this.camControls.update(delta);
 
     var mousePos = this.input.activePointer.positionToCamera(this.cameras.main);
     //get world coords of tile mouse is in.
-    var mouseTileX = this.map.worldToTileX(mousePos.x);
-    var mouseTileY = this.map.worldToTileY(mousePos.y);
+    var mouseTileX = Math.max(Math.min(this.map.worldToTileX(mousePos.x), this.map.width-1), 0);
+    var mouseTileY = Math.max(Math.min(this.map.worldToTileY(mousePos.y), this.map.height-1), 0);
     this.marker.x = this.map.tileToWorldX(mouseTileX);
     this.marker.y = this.map.tileToWorldY(mouseTileY);
 
@@ -177,8 +219,14 @@ gameScene.update = function(time, delta) {
     this.previousSelectedTilePos.y = mouseTileY;
 
     //place wire
-    if (this.input.manager.activePointer.isDown) this.toggleWallAt(mouseTileX, mouseTileY);
+    if (this.input.manager.activePointer.isDown && 
+        this.map.worldToTileX(mousePos.x) < this.map.width && 
+        this.map.worldToTileY(mousePos.y) < this.map.height) 
+            this.placeRoomTileAt(mouseTileX, mouseTileY);
+
     this.timeSinceTilePlaced += 1;
+
+    //update wires
 
     //move player
     this.player.update();
@@ -186,53 +234,179 @@ gameScene.update = function(time, delta) {
         this.toggleWireAt(this.map.worldToTileX(this.player.x), this.map.worldToTileY(this.player.y));
         this.timeSinceTilePlaced = 0;
     }
+
     if (this.keys.UP.isDown || this.keys.W.isDown) this.player.move('N');
     else if (this.keys.DOWN.isDown || this.keys.S.isDown) this.player.move('S');
     else if (this.keys.LEFT.isDown || this.keys.A.isDown) this.player.move('E');
     else if (this.keys.RIGHT.isDown || this.keys.D.isDown) this.player.move('W');
 };
 
-gameScene.toggleWallAt = function (x, y) {
+gameScene.placeRoomTileAt = function (x, y) {
     if (this.timeSinceTilePlaced <= this.tilePlaceCooldown) return;
-    let tile = this.map.getTileAt(x, y, true, 'room');
-    if (tile.index == 1) this.map.putTileAt(0, x, y, false, 'room');
-    else this.map.putTileAt(1, x, y, false, 'room');
+    this.map.putTileAt(this.drawTile, x, y, false, 'room');
     this.timeSinceTilePlaced = 0;
 };
 
 gameScene.toggleWireAt = function(x, y) {
-    if (this.map.getTileAt(x, y) != null) this.map.putTileAt(-1, x, y, false, 'wires');
-    else {
-        this.map.putTileAt(0, x, y, false, 'wires').tint = this.colors.unpowered_blue;
-        // this.map.getTileAt(x, y).tint = Phaser.Display.Color.GetColor(232,143,61);
+    this.map.setLayer('wires');
+    if (this.map.getTileAt(x, y) != null) {
+        this.map.getTileAt(x, y).break();
     }
-    this.updateWireAt(x, y);
-    this.updateWireAt(x+1, y);
-    this.updateWireAt(x-1, y);
-    this.updateWireAt(x, y+1);
-    this.updateWireAt(x, y-1);
+    else {
+        let tile = this.map.putTileAt(0, x, y)
+        Wire.call(tile);
+    }
 };
 
-gameScene.updateWireAt = function(x, y) {
-    if (this.map.getTileAt(x, y) === null) return;
+//intended to be called on a tile
+function Wire() {
+    this.power = -1; //higher = farther from source, -1 = unpowered
+    this.north = false;
+    this.south = false;
+    this.east = false;
+    this.west = false;
+
+    this.debugText = gameScene.add.text(this.tilemap.tileToWorldX(this.x+0.5), this.tilemap.tileToWorldY(this.y+0.5), -1, {
+        color: 'black'
+    });
+    this.debugText.setVisible(gameScene.debugModeEnabled);
     
-    var tileDirs = {
+    var connectableTiles = [gameScene.roomTiles.input, gameScene.roomTiles.output];
+    var tileDirectionIndicies = {
         '': 0,
         'N': 1,   'S': 1,    'NS': 1,
         'E': 2,   'W': 2,    'EW': 2,
-        'ES': 3,  'ESW': 4,  'SW': 5,
-        'NES': 6, 'NESW': 7, 'NSW': 8,
+        'SE': 3,  'SEW': 4,  'SW': 5,
+        'NSE': 6, 'NSEW': 7, 'NSW': 8,
         'NE': 9,  'NEW': 10, 'NW': 11
     }
-    
-    var neighborWires = '';
-    if (this.map.getTileAt(x, y-1) != null) neighborWires += 'N';
-    if (this.map.getTileAt(x+1, y) != null) neighborWires += 'E';
-    if (this.map.getTileAt(x, y+1) != null) neighborWires += 'S';
-    if (this.map.getTileAt(x-1, y) != null) neighborWires += 'W';
-    
-    var tileId = tileDirs[neighborWires];
-    if (tileId == undefined) tileId = 0;
-    
-    this.map.putTileAt(tileId, x, y, false, 'wires');
-};
+
+    this.update = function() {
+        console.log(this.x, this.y);
+
+        //update connections
+        this.north = this.isConnectableWith(this.x, this.y-1);
+        this.south = this.isConnectableWith(this.x, this.y+1);
+        this.east = this.isConnectableWith(this.x+1, this.y);
+        this.west = this.isConnectableWith(this.x-1, this.y);
+
+        this.index = this.getDirectionIndex();
+
+        let oldPower = this.power;
+
+        //check for neighboring outlets
+        let foundPoweringNeighbor = false;
+        this.getRoomNeighbors().forEach(function(tile) {
+            if (tile.index === gameScene.roomTiles.output) {
+                this.power = 0;
+                foundPoweringNeighbor = true;
+            }
+        }, this);
+
+        //get power from neighboring wires
+        this.getWireNeighbors().forEach(function(wire) {
+            if (wire.power === this.power - 1 && wire.power !== -1) foundPoweringNeighbor = true;
+            if (wire.power !== -1  && (wire.power < this.power-1 || this.power === -1)) {
+                this.power = wire.power + 1;
+                foundPoweringNeighbor = true;
+            }
+        }, this);
+
+
+        if (!foundPoweringNeighbor) {
+            this.power = -1;
+        }
+
+        //update neighbors
+        this.getWireNeighbors().forEach(function(wire) {
+            if (this.power === -1) {
+                if (wire.power === oldPower + 1) {
+                    //update wire that was being powered by this
+                    wire.update();
+                }
+            } else {
+                if (wire.power === -1 || wire.power > this.power+1) {
+                    //update wire that should be powered by this
+                    wire.update();
+                }
+            }
+        }, this);
+
+        if (gameScene.debugModeEnabled) this.debugText.setText(this.power);
+
+        //set color
+        if (this.power >= 0) this.tint = gameScene.colors.powered_blue;
+        else this.tint = gameScene.colors.unpowered_blue;
+
+    }
+
+    this.isConnectableWith = function(x, y) {
+        let tile = this.tilemap.getTileAt(x, y, true, 'wires');
+        if (tile.index !== -1) return true;
+
+        tile = this.tilemap.getTileAt(x, y, true, 'room');
+        if (connectableTiles.indexOf(tile.index) != -1) return true;
+        return false;
+    }
+
+    this.getDirectionIndex = function() {
+        let dir = '';
+        if (this.north) dir += 'N';
+        if (this.south) dir += 'S';
+        if (this.east) dir += 'E';
+        if (this.west) dir += 'W';
+        return tileDirectionIndicies[dir];
+    }
+
+    this.getWireNeighbors = function() {
+        var neighbors = [];
+
+        var tile = this.tilemap.getTileAt(this.x, this.y-1, null, 'wires');
+        if (this.north && tile !== null) neighbors.push(tile);
+
+        tile = this.tilemap.getTileAt(this.x, this.y+1, null, 'wires');
+        if (this.south && tile !== null) neighbors.push(tile);
+
+        tile = this.tilemap.getTileAt(this.x+1, this.y, null, 'wires');
+        if (this.east && tile !== null) neighbors.push(tile);
+
+        tile = this.tilemap.getTileAt(this.x-1, this.y, null, 'wires');
+        if (this.west && tile !== null) neighbors.push(tile);
+
+        return neighbors;
+    }
+
+    this.getRoomNeighbors = function() {
+        var neighbors = [];
+
+        var tile = this.tilemap.getTileAt(this.x, this.y-1, null, 'room');
+        if (this.north && tile !== null) neighbors.push(tile);
+
+        tile = this.tilemap.getTileAt(this.x, this.y+1, null, 'room');
+        if (this.south && tile !== null) neighbors.push(tile);
+
+        tile = this.tilemap.getTileAt(this.x+1, this.y, null, 'room');
+        if (this.east && tile !== null) neighbors.push(tile);
+
+        tile = this.tilemap.getTileAt(this.x-1, this.y, null, 'room');
+        if (this.west && tile !== null) neighbors.push(tile);
+
+        return neighbors;
+    }
+
+    this.break = function() {
+        this.index = -1;
+        // this.setPower(-1);
+        this.power = -1;
+        this.getWireNeighbors().forEach(function(wire) {
+            wire.update();
+        });
+        this.tilemap.putTileAt(-1, this.x, this.y, null, 'wires');
+        this.debugText.destroy();
+    }
+
+    this.update();
+    this.getWireNeighbors().forEach(function(wire) {
+        wire.update();
+    });
+}
